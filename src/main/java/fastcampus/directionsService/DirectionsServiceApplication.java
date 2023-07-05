8b6cf4d071c8edc30e3cf18c620234f1a1f90ff8
dgjinsu <rlawlstn3206@naver.com>
2023-07-05 19:59:35 +0900

    first commit

---
+++ src/main/java/fastcampus/directionsService/DirectionsServiceApplication.java
@@ -0,0 +1,13 @@
+package fastcampus.directionsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectionsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fastcampus/directionsService/DirectionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fastcampus.directionsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
